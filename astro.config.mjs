a57502b816d44f5c4333729afba444638c48c29b
chobotek <mladek.milan@gmail.com>
2025-09-04 17:35:09 +0200

    feat: meta + sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://abcd.cz",
+  site: "https://usmevproadama.cz",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
